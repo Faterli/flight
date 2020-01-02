8876422c7064c941e15696f8a117d0f89539208f
faterli <faterlihua@gmail.com>
2020-01-03 01:18:19 +0800

    no message

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://v2mf.com">
+ <a href="https://v2mf.com" target=_blank>
+  V2mf
+  </a>
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
